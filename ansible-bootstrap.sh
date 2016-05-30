ad034fdfefaaa0e91f453aaa6ec7e2f2072d7abb
Félix Archambault <fel.archambault@gmail.com>
2016-05-30 16:32:06 -0400

    improve ansible-bootstrap.sh script to install ansible from ppa on ubuntu fix package detection

--- ansible-bootstrap.sh
+++ ansible-bootstrap.sh
@@ -13,18 +13,29 @@ fi
 echo "found ${PKG_MANAGER} !"
 echo
 
-if [ -x $(which git) ]; then
+if [ -x "$(which git)" ]; then
   echo Git is already installed.
+  echo
 else
   echo Git is not installed. Installing...
   sudo $PKG_MANAGER install -y git
 fi
 
-if [ -x $(which ansible) ] ; then
+if [ -x "$(which ansible)" ] ; then
   echo Ansible is already installed.
+  echo
 else
   echo Ansible is not installed. Installing...
-  sudo $PKG_MANAGER install -y ansible
-fi
 
+  # install from ppa on ubuntu
+  if [ $(hostnamectl  | grep -o "Ubuntu")  = "Ubuntu" ]; then
+    sudo apt-get install software-properties-common
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get install ansible
+  else
+    # install with package manager
+    sudo $PKG_MANAGER install -y ansible
+  fi
+fi
 echo '...all done'
